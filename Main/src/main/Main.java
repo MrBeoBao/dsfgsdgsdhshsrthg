9e45dd75d717634a663dac15562b9a499feeefa1
MrBeoBao <kinnamphuoc2@gmail.com>
2026-04-19 22:42:44 +0700

    Update Main.java

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -4,8 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Main extends JFrame implements ActionListener {
-    JTextField display;
-    double num1 = 0, num2 = 0, result = 0;
-    char operator;
 
+//ddddd
 }
